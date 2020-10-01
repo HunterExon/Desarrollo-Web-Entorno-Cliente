e89ebb914a6308954f375741e410a07b24e09e87
HunterExon <juangccd@gmail.com>
2020-10-01 19:08:17 +0200

    Repo de DEWC

---
+++ Ejercicio 1_Numeros_JGC/holamundo.js
@@ -0,0 +1,19 @@
+/*
+•Crea un programa en el que crees 5 variables numéricas (entero, decimal,  científico, octal y hexadecimal).
+•A las variables les asignarás los siguientes números: 1357, 1357, 135e7,  01357 y 0x1357.
+•Muestra con 5 alerts su valor, escribiendo la siguiente sentencia: alert (“Número entero” + entero);
+•Comenta el código indicando el nombre del ejercicio y tu nombre en la parte  superior, y los comentarios adicionales que estimes necesarios.
+*/
+var entero, decimal,  científico, octal, hexadecimal;
+
+entero=1357;
+decimal=1,357;
+cientifico=135e7;
+octal=01357;
+hexadecimal=0x1357;
+
+alert("Numero entero "+entero);
+alert("Numero decimal "+decimal);
+alert("Numero cientfico "+cientifico);
+alert("Numero octal "+octal);
+alert("Numero hexadecimal "+hexadecimal);
---
+++ Ejercicio 2_Cadenas_JGC/holamundo.js
@@ -0,0 +1,21 @@
+/*
+•Crea un programa en el que crees 4 variables de tipo cadena con los  siguientes valores: “Hola”, “7”, “13”, y “Adios”.
+•Muestra en un alert una frase que incluya comillas simples.
+•Muestra en un alert que ocupe una línea las variables 1ª y 4ª separadas por  un salto de línea.
+•Muestra en un alert la suma de las variables 2ª y 3ª.
+•Muestra en un alert la suma de todas las variables.
+•Comenta el código indicando el nombre del ejercicio y tu nombre en la parte  superior, y los comentarios adicionales que estimes necesarios.
+*/
+var entero, decimal,  científico, octal, hexadecimal;
+
+entero=1357;
+decimal=1,357;
+cientifico=135e7;
+octal=01357;
+hexadecimal=0x1357;
+
+alert("Numero entero "+entero);
+alert("Numero decimal "+decimal);
+alert("Numero cientfico "+cientifico);
+alert("Numero octal "+octal);
+alert("Numero hexadecimal "+hexadecimal);
